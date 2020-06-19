1e543d7aac61dbd6c0ff2640b3f04bc8421f7190
ccedacero <cristian.cedacero@gmail.com>
2020-06-19 14:42:36 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(arr, event) {
+    let thanks = []
+    for (let i = 0; i < arr.length; i++) {
+        thanks.push(`Thank you, ${arr[i]}, for the wonderful ${event} gift!`);
+    }
+    return thanks
+}
+
+
+function countDown(num) {
+    for (let i = 0; i <= num; i++) {
+        console.log(i)
+    }
+}
